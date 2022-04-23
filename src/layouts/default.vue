79cd458792fb4af536b604553ec5ebca94e4514c
hyodoblog <yusei.hyodo@youlib.co.jp>
2022-04-23 14:18:56 +0900

    init vuetify

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,9 +2,15 @@
 </script>
 
 <template>
-  <div>
-    <slot></slot>
-  </div>
+  <v-app>
+    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
+    <v-app-bar color="grey-lighten-2"></v-app-bar>
+    <v-main>
+      <v-card height="200px">
+        <slot></slot>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
 <style lang="scss">
