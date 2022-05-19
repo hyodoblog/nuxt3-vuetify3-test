e4611330e5c0f5f665eaaac2c73678de67a1ec71
hyodoblog <yusei.hyodo@gmail.com>
2022-05-19 18:02:55 +0900

    リファクタリング

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,5 @@ export default defineNuxtConfig({
   srcDir: 'src',
   build: {
     transpile: ['vuetify']
-},
+  },
 })
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,8 +3,6 @@
 
 <template>
   <v-app>
-    <v-navigation-drawer color="grey-darken-2" permanent></v-navigation-drawer>
-    <v-app-bar color="grey-lighten-2"></v-app-bar>
     <v-main>
       <v-card height="200px">
         <slot></slot>
